c4c5a43f6a2f3ef62002dc6a25c444e7dcacb0fe
Dricovw <112882481+Dricovw@users.noreply.github.com>
2023-11-20 19:17:32 +0100

    save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,33 @@
 <script>
   import '../app.css';
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "../lib/components/Home.svelte";
+  import Profile from "../lib/components/Profile.svelte";
 </script>
 
 <div class="flex flex-col h-screen justify-between">
   <main>
     <slot />
     <!-- here the page content of +page.svelte is loaded-->
+    
   </main>
 
   <footer>
+    <Router>
+      <Route path="/home" component={Home} />
+      <Route path="/profile" component={Profile} />
+      <nav class="grid grid-cols-7 gap-4 p-4 md:p-8 rounded-md shadow-md">
+        <Link to="/home" class="bg-gray-300 text-base md:text-xl lg:text-2xl">Home</Link>
+        <Link to="/profile" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link>
+        <Link to="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</Link>
+        <Link to="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Calender</Link>
+        <Link to="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</Link>
+        <Link to="/profile" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link>
+        <Link to="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Self-help</Link>
+      </nav>
+    
+
+    </Router>
     <p class="p-2 text-center">Made with &#x2764;</p>
   </footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script>
-  import Home from "$lib/components/Home.svelte";
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "../lib/components/Home.svelte";
+  import Profile from "../lib/components/Profile.svelte";
 </script>
 
-<Home />
+<!-- Your component markup goes here -->
+
+
+
+
+<main>
+  the text
+  <p class="text-xs">the bigger text</p>
+</main>
+<style>
+  /* Your component styles go here */
+</style>
