a7acb0f46f71761c87ce1c92d995b55cd52810bb
Dricovw <112882481+Dricovw@users.noreply.github.com>
2023-11-21 11:27:18 +0100

    login page 50%

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,26 +3,43 @@
   import Home from "../components/Home.svelte";
   import Profile from "../components/Profile.svelte";
 </script>
-<main>
-    <footer>
-        <Router>
-         
-          <nav class="grid grid-cols-7 gap-4 p-4 md:p-8 rounded-md shadow-md">
-            <Link to="/home" class="bg-gray-300 text-sm text-center">Home</Link>
-            <Link to="/profile" class="bg-gray-300 p-1 text-base rounded-md shadow-md text-base">Profile</Link>
-            <Link to="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</Link>
-            <Link to="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Calender</Link>
-            <Link to="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</Link>
-            <Link to="/profile" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link>
-            <Link to="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Self-help</Link>
-          </nav>
-        
-    
-        </Router>
-        <p class="p-2 text-center">Made with &#x2764;</p>
-      </footer>
-</main>
 
+<div class="flex flex-col h-screen justify-between">
+    <footer class="absolute inset-x-0 bottom-0">
+      <Router>
+        <nav class="grid grid-cols-7 gap-4 p-4 md:p-8 rounded-md shadow-md">
+          <Link to="/home" class="bg-gray-300 text-sm text-center">Home</Link>
+          <Link
+            to="/profile"
+            class="bg-gray-300 p-1 text-base rounded-md shadow-md text-base"
+            >Profile</Link
+          >
+          <Link
+            to="/events"
+            class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</Link
+          >
+          <Link
+            to="/calender"
+            class="bg-gray-300 p-1 rounded-md shadow-md text-base"
+            >Calender</Link
+          >
+          <Link
+            to="/stats"
+            class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</Link
+          >
+          <Link
+            to="/profile"
+            class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link
+          >
+          <Link
+            to="/self-help"
+            class="bg-gray-300 p-1 rounded-md shadow-md text-base"
+            >Self-help</Link
+          >
+        </nav>
+      </Router>
+      <p class="p-2 text-center">Made with &#x2764;</p>
+    </footer>    </div>
 <style>
     
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,18 @@
 <script>
-    import { Router, Link, Route } from "svelte-routing";
+  import "../app.css";
+  import NavBar from "$lib/components/NavBar.svelte";
+  import { Router, Link, Route } from "svelte-routing";
 </script>
 
 <div>
-  
   <main>
     <h2>Name</h2>
-    <input type="text">
-
+    <input type="text" />
 
     <h2>Password</h2>
-    <input type="text">
-
-
+    <input type="text" />
 
     <button type="submit">Login</button>
-    
-
-    <footer>
-    <Router>
-     
-      <nav class="grid grid-cols-7 gap-4 p-4 md:p-8 rounded-md shadow-md">
-        <Link to="/home" class="bg-gray-300 text-sm text-center inline-block align-middle">Home</Link>
-        <Link to="/profile" class="bg-gray-300 p-1 text-base rounded-md shadow-md text-base">Profile</Link>
-        <Link to="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</Link>
-        <Link to="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Calender</Link>
-        <Link to="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</Link>
-        <Link to="/profile" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link>
-        <Link to="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Self-help</Link>
-      </nav>
-      </Router>
-    </footer>
-
   </main>
 </div>
 
