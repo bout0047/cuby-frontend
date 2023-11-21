df97c9d17bf4f020507e0a470d6b09579541f9cc
Ignacio-amat <amat0002@hz.nl>
2023-11-21 14:12:32 +0100

    pulling this branch

---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { navigate } from "svelte-routing";
+
+  let username = "";
+  let password = "";
+
+  function handleSubmit() {
+    // Perform login logic here
+
+    // Redirect to /home
+    navigate("/home");
+  }
+</script>
+
+<main>
+  <h1>Login</h1>
+  <form on:submit={handleSubmit}>
+    <label>
+      Username:
+      <input type="text" />
+    </label>
+    <label>
+      Password:
+      <input type="password" />
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+</main>
