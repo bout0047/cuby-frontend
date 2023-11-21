197a89f85452ff0009663d353a02ad1d390bbc3a
Bart Wolters <bartw1999@gmail.com>
2023-11-21 13:33:45 +0100

    Added simple calendar
    Will change following database implementation

--- src/old files/App.svelte
+++ src/old files/App.svelte
@@ -4,6 +4,7 @@
 	import { Router, Link, Route } from "svelte-routing";
   	import Profile from "../../../src/Profile.svelte";
     import Home from "../lib/components/Home.svelte";
+	import Calendar from "../lib/components/Calendar.svelte";
 </script>
 
 
@@ -11,10 +12,12 @@
 	<nav>
 		<Link to="/home">Home</Link>
 		<Link to="/profile">Profile</Link> 
+		<Link to="/calendar">Calendar</Link>
 	  </nav>
 
 	  <Route path="/Home" component={Home} />
 	  <Route path="/profile" component={Profile} />
+	  <Route path="/calendar" component={Calendar} />
   </Router>
 
 <style>
