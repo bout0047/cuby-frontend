49bab57ebc5be12e261718d77b2e6e46e8bef053
Ignacio-amat <amat0002@hz.nl>
2023-11-21 10:38:59 +0100

    very simple login

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,17 +1,28 @@
 <script>
-
+    import { Router, Link, Route } from "svelte-routing";
+  import Home from "../components/Home.svelte";
+  import Profile from "../components/Profile.svelte";
 </script>
 <main>
-    <div class="top-right">
-        <a href="Profile.svelte"><i class="fa-solid fa-user" style="padding-right: 8px;"></i></a>
-        <a href="Settings.svelte"><i class="fa-solid fa-gear"></i></a>
-    </div>
+    <footer>
+        <Router>
+         
+          <nav class="grid grid-cols-7 gap-4 p-4 md:p-8 rounded-md shadow-md">
+            <Link to="/home" class="bg-gray-300 text-sm text-center">Home</Link>
+            <Link to="/profile" class="bg-gray-300 p-1 text-base rounded-md shadow-md text-base">Profile</Link>
+            <Link to="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</Link>
+            <Link to="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Calender</Link>
+            <Link to="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</Link>
+            <Link to="/profile" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link>
+            <Link to="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Self-help</Link>
+          </nav>
+        
+    
+        </Router>
+        <p class="p-2 text-center">Made with &#x2764;</p>
+      </footer>
 </main>
 
 <style>
-    .top-right {
-        text-align: right;
-        font-size: x-large;
-        padding-right: 5px;
-    }
+    
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,18 @@
 <script>
-  import '../app.css';
-  import { Router, Link, Route } from "svelte-routing";
-  import Home from "../lib/components/Home.svelte";
-  import Profile from "../lib/components/Profile.svelte";
+  
 </script>
 
-<div class="flex flex-col h-screen justify-between">
+<div>
+  
   <main>
-    <slot />
-    <!-- here the page content of +page.svelte is loaded-->
-    <Router>
-    <Route path="/home" component={Home} />
-    <Route path="/profile" component={Profile} />
-    </Router>
+    <h2>Name</h2>
+    <input type="text">
+
+    <h2>Password</h2>
+    <input type="text">
+
+    <button type="submit">Login</button>
   </main>
-
-  <footer>
-    <Router>
-     
-      <nav class="grid grid-cols-7 gap-4 p-4 md:p-8 rounded-md shadow-md">
-        <Link to="/home" class="bg-gray-300 text-sm text-center">Home</Link>
-        <Link to="/profile" class="bg-gray-300 p-1 text-base rounded-md shadow-md text-base">Profile</Link>
-        <Link to="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</Link>
-        <Link to="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Calender</Link>
-        <Link to="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</Link>
-        <Link to="/profile" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Profile</Link>
-        <Link to="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Self-help</Link>
-      </nav>
-    
-
-    </Router>
-    <p class="p-2 text-center">Made with &#x2764;</p>
-  </footer>
 </div>
 
 <style>
