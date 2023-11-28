a48e61ea64a50e48cb745e065a2d3d6f0852db25
Dricovw <112882481+Dricovw@users.noreply.github.com>
2023-11-28 14:51:38 +0100

    updated navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -4,12 +4,12 @@
 
     <footer>         
           <nav class="grid grid-cols-6 gap-4 p-4 md:p-8 rounded-md shadow-md">
-            <a href="/home" class="bg-gray-300 text-sm text-center">Home</a>
-            <a href="/profile" class="bg-gray-300 p-1 text-base rounded-md shadow-md text-base">Profile</a>
-            <a href="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Events</a>
-            <a href="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Calender</a>
-            <a href="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Stats</a>
-            <a href="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-base">Self-help</a>
+            <a href="/home" class="bg-gray-300 text-3xl text-center"><i class="fa-solid fa-house"></i></a>
+            <a href="/profile" class="bg-gray-300 p-1 text-3xl rounded-md shadow-md text-3xl"><i class="fa-solid fa-user"></i></a>
+            <a href="/events" class="bg-gray-300 p-1 rounded-md shadow-md text-3xl"><i class="fa-brands fa-elementor"></i></a>
+            <a href="/calender" class="bg-gray-300 p-1 rounded-md shadow-md text-3xl"><i class="fa-solid fa-calendar-days"></i></a>
+            <a href="/stats" class="bg-gray-300 p-1 rounded-md shadow-md text-3xl"><i class="fa-solid fa-chart-simple"></i></a>
+            <a href="/self-help" class="bg-gray-300 p-1 rounded-md shadow-md text-3xl"><i class="fa-solid fa-book-open"></i></a>
           </nav>
         <p class="p-2 text-center">Made with <i class="fa-solid fa-cube" style="color: pink;"></i></p>
 
