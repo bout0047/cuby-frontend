ffdcb157e89dd951c2ce9adc4e26efa9c6149c61
Ignacio-amat <amat0002@hz.nl>
2023-11-28 14:40:12 +0100

    user can see upcoming event

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,118 +1,105 @@
 <script>
-	import "@fortawesome/fontawesome-free/js/all.js";
-    import NavBar from '$lib/components/NavBar.svelte';
+  import '@fortawesome/fontawesome-free/js/all.js';
+  import NavBar from '$lib/components/NavBar.svelte';
   import TopNav from '$lib/components/TopNav.svelte';
 </script>
 
 <main>
-  <TopNav/>
-    <div id="cubyconnect" class="cubyconnect">
-        <a href="cubyconnect">
-            <h1>Hello ! <i class="fa-solid fa-cube"></i></h1>
-        </a>
-        <h2>Connect to cuby here!</h2>
-    </div>
+  <TopNav />
 
-    <div class="icon-container">
-        <div class="icon-row">
-            <div class="icon profile">
-                <a href="/profile">profile</a><br />
-                <img src="img/profile.png" alt="profile"/>
-            </div>
+  <section>
+    <h1 class="text-left ml-2 underline font-bold">Next Event:</h1>
+    <div class="left-2 border rounded">
+      <div class="grid grid-cols-2 m-3 text-left">
+        <div class=" gap-4">
+          <div>
+            <h2>Event Name</h2>
+          </div>
+          <div>
+            <h3>Event Time</h3>
+          </div>
+          <div>
+            <h3>Event Location</h3>
+          </div>
         </div>
-        <div class="icon-row multi-icons">
-            <div class="icon events">
-                <a href="Events.svelte">events</a><br />
-                <img src="img/events.png" alt="events"/>
-            </div>
-            <div class="icon calendar">
-                <a href="Calendar.svelte">calendar</a><br />
-                <img src="img/calendar.png" alt="calendar"/>
-            </div>
+        <div class="w-12 ml-32">
+          <img src="src/img/calender.png" class="w-10 h-10 float-right mr-4" />
         </div>
-        <div class="icon-row multi-icons">
-            <div class="icon stats">
-                <a href="Stats.svelte">stats</a><br />
-                <img src="img/stats.png" alt="stats"/>
-            </div>
-            <div class="icon self-help">
-                <a href="Self-help.svelte">self-help</a><br />
-                <img src="img/self-help.png" alt="self help"/>
-            </div>
-        </div>
+      </div>
     </div>
+  </section>
 
-    <footer>
-        <NavBar/>
-    </footer>
+  <footer>
+    <NavBar />
+  </footer>
 </main>
 
+<style>
+  :global(body) {
+    background-color: #87abab;
+    margin: 0;
+    overflow-x: hidden;
+  }
 
+  main {
+    padding: 1em;
+    text-align: center; /* Center content */
+  }
 
-  <style>
-    :global(body) {
-        background-color: #87abab;
-        margin: 0;
-        overflow-x: hidden;
-    }
+  .cubyconnect {
+    background-color: darkblue;
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+  }
 
-    main {
-        padding: 1em;
-        text-align: center; /* Center content */
-    }
+  .icon-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
 
-    .cubyconnect {
-        background-color: darkblue;
-        color: white;
-        padding: 20px;
-        border-radius: 10px;
-        margin-bottom: 20px;
-    }
+  .icon-row {
+    display: flex;
+    justify-content: center;
+    width: 100%; /* Make sure this takes up the full width */
+  }
 
-    .icon-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 20px;
-    }
+  .icon-row.multi-icons {
+    justify-content: space-around;
+  }
 
-    .icon-row {
-        display: flex;
-        justify-content: center;
-        width: 100%; /* Make sure this takes up the full width */
-    }
+  .icon {
+    background-color: dimgray;
+    border-radius: 20px;
+    padding: 10px;
+    width: calc(
+      50% - 40px
+    ); /* Adjust this to make sure two icons fit in one row */
+    margin: 10px; /* Center and space out icons */
+  }
 
-    .icon-row.multi-icons {
-        justify-content: space-around;
-    }
+  .icon img {
+    max-width: 100%; /* Responsive image sizing */
+    height: auto;
+  }
 
+  /* Responsive adjustments */
+  @media (max-width: 768px) {
     .icon {
-        background-color: dimgray;
-        border-radius: 20px;
-        padding: 10px;
-        width: calc(50% - 40px); /* Adjust this to make sure two icons fit in one row */
-        margin: 10px; /* Center and space out icons */
+      width: calc(100% - 20px); /* Full width on smaller screens */
+    }
+  }
+
+  @media (max-width: 412px) {
+    .cubyconnect h1 {
+      font-size: 1.5em; /* Adjust font size for small screens */
     }
 
-    .icon img {
-        max-width: 100%; /* Responsive image sizing */
-        height: auto;
+    .cubyconnect h2 {
+      font-size: 1.2em; /* Adjust sub-header font size for small screens */
     }
-
-    /* Responsive adjustments */
-    @media (max-width: 768px) {
-        .icon {
-            width: calc(100% - 20px); /* Full width on smaller screens */
-        }
-    }
-
-    @media (max-width: 412px) {
-        .cubyconnect h1 {
-            font-size: 1.5em; /* Adjust font size for small screens */
-        }
-
-        .cubyconnect h2 {
-            font-size: 1.2em; /* Adjust sub-header font size for small screens */
-        }
-    }
+  }
 </style>
