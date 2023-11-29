8b59b89c219dd7243c6d6dd63511b5adaf070803
Dricovw <112882481+Dricovw@users.noreply.github.com>
2023-11-29 12:58:14 +0100

    Navbar implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
       'white': '#ffffff',
       'blue': '#090C9B',
     },
+    shadow: {
+    'shadow-2xl': 'box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);',
+    },
     extend: {},
   },
   plugins: [],
