1a93d3dc1d5508f363c226ae5f1a8fbe8632c9d6
Dawid <czub0002@hz.nl>
2023-12-01 14:27:11 +0100

    working event fetch

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -1,23 +1,22 @@
 <script>
-export let events = [];
+  export let events = [];
 
-export async function load() {
-  try {
-    const response = await fetch('https://apigateaway:3011/events');
+  export async function load() {
+    try {
+      const response = await fetch('http://localhost:3011/events');
 
-    if (!response.ok) {
-      console.error('Error fetching events:', response.status, response.statusText);
-      throw new Error('Failed to fetch events');
+      if (!response.ok) {
+        console.error('Error fetching events:', response.status, response.statusText);
+        throw new Error('Failed to fetch events');
+      }
+
+      events = await response.json();
+    } catch (error) {
+      console.error('Error fetching events:', error.message);
     }
-
-    events = await response.json();
-  } catch (error) {
-    console.error('Error fetching events:', error.message);
   }
-}
-
 </script>
 
 <main>
-  Hello Poland
+{events}
 </main>
