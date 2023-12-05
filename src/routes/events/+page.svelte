3dfafb8aff322afe050c8ec948a1c7099326bf9d
ilyasooo <112753422+bout0047@users.noreply.github.com>
2023-12-05 11:51:25 +0100

    Search bar working

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,37 +1,56 @@
 <script>
-  import '@fortawesome/fontawesome-free/js/all.js';
   import NavBar from '$lib/components/NavBar.svelte';
-  import TopNav from '$lib/components/TopNav.svelte';  import { onMount } from 'svelte';
+  import TopNav from '$lib/components/TopNav.svelte';
+  import { onMount } from 'svelte';
 
-  export let events = [];
+  let events = [];
+  let searchQuery = '';
 
   onMount(async () => {
     try {
       const response = await fetch('http://localhost:3011/events');
-
       if (!response.ok) {
-        console.error('Error fetching events:', response.status, response.statusText);
         throw new Error('Failed to fetch events');
       }
-
       events = await response.json();
     } catch (error) {
       console.error('Error fetching events:', error.message);
     }
   });
+
+  $: filteredEvents = searchQuery
+    ? events.filter(event => 
+        event.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    : events;
 </script>
 
 <main>
-  {#if events.length > 0}
-    <ul>
-      {#each events as event (event.id)}
-        <li>{event.name}</li>
+  <TopNav />
+  <input type="text" bind:value={searchQuery} placeholder="Search events..." class="search-bar" />
+
+  {#if filteredEvents.length > 0}
+    <ul class="events-list">
+      {#each filteredEvents as event (event.id)}
+        <li class="event-item">{event.name}</li>
       {/each}
     </ul>
   {:else}
-    <p>No events available.</p>
+    <p>No events found.</p>
   {/if}
+
   <footer>
     <NavBar />
-    </footer>
+  </footer>
 </main>
+
+<style>
+  .search-bar {
+    /* Your search bar styles */
+  }
+  .events-list {
+    /* Styles for your events list */
+  }
+  .event-item {
+    /* Styles for each event item */
+  }
+</style>
