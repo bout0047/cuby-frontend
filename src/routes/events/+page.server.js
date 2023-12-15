43c123daf7fad477061a741e6c6a3f9db3c1a782
vladpavlovhz <pavl0008@hz.nl>
2023-12-15 05:31:10 +0100

    Errors for event load

--- src/routes/events/+page.server.js
+++ src/routes/events/+page.server.js
@@ -2,6 +2,11 @@ export const load = async () => {
 
     const fetchEvents = async() => {
         const response = await fetch('http://localhost:3011/events');
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch events data: ${response.statusText}`);
+        }
+
         const eventsData = await response.json();
         return eventsData;
     }
--- src/routes/events/[id]/+page.server.js
+++ src/routes/events/[id]/+page.server.js
@@ -2,6 +2,11 @@ export const load = async ({ params }) => {
 
     const fetchEvent = async() => {
         const response = await fetch(`http://localhost:3011/events/${params.id}`);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch event data: ${response.statusText}`);
+        }
+        
         const eventData = await response.json();
         return eventData;
     }
