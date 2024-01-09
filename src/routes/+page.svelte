cdacb508abef99310d2f7719e3f4a63092772b4c
Ignacio-amat <amat0002@hz.nl>
2024-01-09 15:44:49 +0100

    top half of home page done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,18 +14,18 @@
  
 <main>
     <div>
-        <h1 class="text-5xl text-center decoration-8 mt-20">Cuby</h1>
+        <h1 class="text-5xl text-center decoration-8 mt-20 font-bold">Cuby</h1>
 
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
-          class="cursor-pointer bg-darkestBlue hover:bg-somePaleGreen text-somePaleGreen hover:text-black font-semibold w-1/2 text-center justify-center m-auto mt-20 py-2 px-4 rounded"
+          class="cursor-pointer bg-royalBlue hover:bg-somePaleGreen text-somePaleGreen hover:text-black font-semibold w-1/2 text-center justify-center m-auto mt-20 py-2 px-4 rounded"
           on:click={ () => goto('./login')}
         >
           Login
         </div>
  
         <div
-          class="cursor-pointer bg-darkestBlue hover:bg-somePaleGreen text-somePaleGreen hover:text-black font-semibold w-1/2 text-center justify-center m-auto mt-20 py-2 px-4 rounded"
+          class="cursor-pointer bg-royalRed hover:bg-somePaleGreen text-somePaleGreen hover:text-black font-semibold w-1/2 text-center justify-center m-auto mt-20 py-2 px-4 rounded"
           on:click={ () => goto('./register')}
         >
           Register
