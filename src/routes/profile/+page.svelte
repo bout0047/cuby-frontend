0a27058885eae91afb351218cdf5aa47eb17088c
Dricovw <112882481+Dricovw@users.noreply.github.com>
2024-01-12 14:24:33 +0100

    colour

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,7 +4,7 @@
    import { onMount } from "svelte";
    let profilepicture = "../src/img/stokstraart.png";
    export let profiles = [];
-   let id = 3;
+   let id = 1;
 
    onMount(async () => {
       try {
@@ -28,48 +28,19 @@
 
 <main class="container mx-auto p-4 bg-090C9B relative">
    {#if profiles.length > 0}
-   <section class="text-center relative">
-      <button on:click={() => {
-            window.location.href = "/register/edit";}}
-         class="corner-button absolute bg-royalBlue top-0 text-wrap text-sm right-0 p-2 px-4 text-white rounded-full focus:outline-none focus:ring focus:border-blue-300 transition-all duration-300 ease-in-out"
-         >Edit Profile</button>
-         <button on:click={() => {
-            window.location.href = "/profile/goals";}}
-         class="corner-button absolute bg-royalBlue top-0 text-sm right-72 text-nowrap p-2 px-4 text-white rounded-full "
-         >Edit Goals</button>
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img
-         src={profilepicture}
-         alt="Profile Picture"
-         class="rounded-full shadow-md w-56 h-56 mx-auto mb-4 mt-2"
-      />
-      
-      <h1 class="text-3xl font-bold">{profiles[id].name}</h1>
-      <p class="text-gray-600">Email: {profiles[id].email}</p>
-   </section>
-<section>
-</section>
-   <section class="mt-6">
-      <h2 class="text-4xl font-semibold mb-2">Goals</h2>
-      <p class="text-2xl font-semibold">I want to focus on:</p> <div class="text-xl font-medium">{profiles[id].goals[0]}</div>
-      <p class="text-2xl font-semibold">I will do this by:</p> <div class="text-xl font-medium">{profiles[id].goals[1]}</div>
-      <p class="text-2xl font-semibold">If I stress to much I will:</p> <div class="text-xl font-medium">{profiles[id].goals[2]}</div>
-   </section>
       <section class="text-center relative">
          <button
             on:click={() => {
                window.location.href = "/profile/edit";
             }}
-            class="corner-button absolute bg-royalBlue top-0 text-wrap text-sm right-0 p-2 px-4 text-white rounded-full focus:outline-none focus:ring focus:border-blue-300 transition-all duration-300 ease-in-out"
-            >Edit Profile</button
-         >
+            class="corner-button absolute bg-royalBlue top-0  right-2 text-wrap text-sm right-0 p-2 px-4 text-cream rounded-full"
+            >Edit Profile</button>
          <button
             on:click={() => {
                window.location.href = "/profile/goals";
             }}
-            class="corner-button absolute bg-royalBlue top-0 text-sm right-72 text-nowrap p-2 px-4 text-white rounded-full"
-            >Edit Goals</button
-         >
+            class="corner-button absolute bg-royalBlue top-0 text-sm right-72 text-nowrap p-2 px-4 text-cream rounded-full"
+            >Edit Goals</button>
          <!-- svelte-ignore a11y-img-redundant-alt -->
          <img
             src={profilepicture}
