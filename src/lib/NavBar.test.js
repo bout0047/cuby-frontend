feb6e7a51ca0cdfdbebdd601d8159300d79ae1a4
Ignacio-amat <amat0002@hz.nl>
2024-01-13 15:22:03 +0100

    finished Navbar test

--- src/lib/NavBar.test.js
+++ src/lib/NavBar.test.js
@@ -1,6 +1,18 @@
 import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
 import NavBar from './components/NavBar.svelte';
+import { navigate } from 'svelte-routing';
 
-test('should render the NavBar component', () => {
+test('when events icon is pressed it should take the user to events page', async () => {
   render(NavBar);
+  const eventsIcon = screen.getByTestId('events-icon');
+
+  // Pressing the events icon
+  userEvent.click(eventsIcon);
+
+  // Going to the events page
+  await navigate('/events');
+
+  // Make sure we are on the events page
+  expect(window.location.href).toBe('http://localhost:3000/events');
 });
