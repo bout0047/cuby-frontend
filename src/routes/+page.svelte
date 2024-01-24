ce6ef2b58b51816132869737e68a6d6e70337ced
Dricovw <112882481+Dricovw@users.noreply.github.com>
2024-01-24 15:39:01 +0100

    added testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,6 +20,7 @@
         <div
           class="cursor-pointer bg-darkestBlue hover:bg-somePaleGreen text-somePaleGreen hover:text-black font-semibold w-1/2 text-center justify-center m-auto mt-20 py-2 px-4 rounded"
           on:click={ () => goto('./login')}
+          data-testid='login'
         >
           Login
         </div>
@@ -27,6 +28,7 @@
         <div
           class="cursor-pointer bg-darkestBlue hover:bg-somePaleGreen text-somePaleGreen hover:text-black font-semibold w-1/2 text-center justify-center m-auto mt-20 py-2 px-4 rounded"
           on:click={ () => goto('./register')}
+          data-testid='register'
         >
           Register
         </div>
